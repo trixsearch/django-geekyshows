a211191fa48e184d163adfe7841e020563180759
Abhinav1 Sahu <Abhinav1.Sahu@ril.com>
2026-02-05 19:42:36 +0530

    adding Model Admit Registration for learning

--- learning/app/admin.py
+++ learning/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile,Result
 
-# Register your models here.
+# # Ye Simple Tareeka hai Model ko register karne ka isme __str__ method sey cheezein dikhengi
+# admin.site.register(Profile)
+# yaha per jis bhi model ko likhenge voh Admin Portal mein App Name k neeche likha aa jayega
+admin.site.register(Result)
+
+# Jab humko ek sey zyada fields register karni padhti hai toh hum ModelAdmin Class ka use karte hai
+# isme pahle toh Class banakar fields define karni padhti hai 
+# fir usko register karna padhta hai 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=('id','name','roll','city')
+
+admin.site.register(Profile,ProfileAdmin)
--- learning/app/models.py
+++ learning/app/models.py
@@ -3,6 +3,19 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name=models.CharField(max_length=100)
+    roll=models.IntegerField()
     email=models.EmailField(max_length=200)
     city=models.CharField(max_length=100)
     phone=models.IntegerField() 
+
+    # def __str__(self):
+    #     # yaha jo bhi value dalenge , us naam sey entry dikhegi models k ander Admin Portal me
+    #     return str(self.name)
+    
+class Result(models.Model):
+    stu_class=models.CharField(max_length=50)
+    marks=models.IntegerField()
+    # agar is tarike sey nahi karna hai toh Model Admin Class admin.py mein likh dete hai
+    # yaha mein Simple or normal way sey karke dikhaya hu
+    def __str__(self):
+        return str(self.stu_class)
